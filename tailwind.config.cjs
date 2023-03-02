caae36706bf94bb22aff5c78b40c633acde3a227
Kerem Aydemir <keremaydemir59@gmail.com>
2023-03-02 16:57:25 +0300

    slight changes to initial setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1240px',
+        '2xl': '1496px',
+      },
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
